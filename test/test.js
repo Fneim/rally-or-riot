2af580404d675395f9758e96ce6809f2716dc262
Jeff Palin <jeffpalin@gmail.com>
2017-10-04 16:56:47 -0700

    backup testing and package.json

--- test/test.js
+++ test/test.js
@@ -49,6 +49,21 @@ describe("Sign-In Page Loading Test", function() {
     });
 });
 
+// Explore Page test
+
+describe("Home Page Loading Test", function() {
+
+    var url = "http://localhost:3100/explore";
+
+    it("returns status 200", function() {
+        request(url, function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+        });
+    });
+});
+
+// Sign Up and Create User in Database
+
 describe("User sign up", function() {
     var url = "http://localhost:3100/signup";
     it("signs up a user", function() {
@@ -65,6 +80,7 @@ describe("User sign up", function() {
         }, function(error, body, response) {
             if (error) throw error;
             expect(response.statusCode).to.equal(302);
+
         });
     });
 });
